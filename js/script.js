e9bc215ef4201329a56c6297e664b3ce05a339e4
Aiperi Myimanbai <aiperi.myimanbaikyzy.pls@gmail.com>
2022-03-03 19:25:09 +0600

    header_mobile

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggler = document
+  .querySelector(".menu-toggler")
+  .addEventListener("click", function () {
+    menu.classList.toggle("menu-show");
+  });
+
+const menu = document.querySelector(".menu");
