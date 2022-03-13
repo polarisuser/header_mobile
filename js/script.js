4ee3edc47e3df6aaba24abcf6c8d4fc9e535b557
Aiperi Myimanbai <aiperi.myimanbaikyzy.pls@gmail.com>
2022-03-13 12:28:09 +0600

    finished

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const toggler = document
-  .querySelector(".menu-toggler")
-  .addEventListener("click", function () {
-    menu.classList.toggle("menu-show");
-  });
+const nav = document.querySelector(".nav");
+const navbar = document.querySelector(".navbar");
+const burgerLink = document.querySelector(".burger__link");
+const navLink = nav.querySelectorAll(".nav__link");
 
-const menu = document.querySelector(".menu");
+burgerLink.addEventListener("click", function () {
+  nav.classList.toggle("nav-show");
+  navbar.after(nav);
+});
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", function () {
+    let current = document.querySelectorAll(".active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+
+    if (window.innerWidth <= 840) {
+      nav.classList.toggle("nav-show");
+    }
+  });
+}
